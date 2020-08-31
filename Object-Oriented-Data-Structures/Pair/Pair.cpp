936ab999c11fc2b8e993777a638cd1d47898a8ee
vonewman <vonewman7@gmail.com>
2020-08-31 12:46:39 -0400

    add Object-Oriented-Data-Structures/

---
+++ Object-Oriented-Data-Structures/Pair/Pair.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+class Pair {
+ public:
+	 int a;
+	 int b;
+
+	 int sum()
+	 {
+		 return (a + b);
+	 }
+};
+
+
+int main() {
+	Pair p;
+	p.a = 100;
+	p.b = 200;
+
+	if (p.a + p.b == p.sum()) {
+		cout << "Success!" << endl;
+	} else {
+		cout << "p.sum() returns " << p.sum() << " instead of " 
+			 << (p.a + p.b) << endl;
+	}
+	return 0;
+}
+
+
